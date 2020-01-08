26ff8a684dca2002bf23896272bd2b10fcec645b
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-01-08 10:29:29 +0800

    Update 1.php

--- theme/1.php
+++ theme/1.php
@@ -3,9 +3,6 @@
 <html lang="<?php echo $constStr['language']; ?>">
 <head>
     <title><?php echo $pretitle;?> - <?php echo $_SERVER['sitename'];?></title>
-    <!--
-        github ： https://github.com/qkqpttgf/OneManager-php
-    -->
     <meta charset=utf-8>
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1">
