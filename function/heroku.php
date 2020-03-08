a4ec3a85b9b4a0468cd2bd0d5ac453a742629fb6
Mr.Lin <lsx0044@gmail.com>
2020-03-08 16:59:25 +0800

    Update heroku.php

--- function/heroku.php
+++ function/heroku.php
@@ -93,141 +93,6 @@ function setConfig($arr, $disktag = '')
     return setHerokuConfig($tmp, getConfig('function_name'), getConfig('APIKey'));
 }
 
-function get_refresh_token()
-{
-    global $constStr;
-    global $CommonEnv;
-    foreach ($CommonEnv as $env) $envs .= '\'' . $env . '\', ';
-    $url = path_format($_SERVER['PHP_SELF'] . '/');
-    if ($_GET['authorization_code'] && isset($_GET['code'])) {
-        $_SERVER['disktag'] = $_COOKIE['disktag'];
-        config_oauth();
-        $tmp = curl_request($_SERVER['oauth_url'] . 'token', 'client_id=' . $_SERVER['client_id'] .'&client_secret=' . $_SERVER['client_secret'] . '&grant_type=authorization_code&requested_token_use=on_behalf_of&redirect_uri=' . $_SERVER['redirect_uri'] .'&code=' . $_GET['code']);
-        if ($tmp['stat']==200) $ret = json_decode($tmp['body'], true);
-        if (isset($ret['refresh_token'])) {
-            $tmptoken = $ret['refresh_token'];
-            $str = '
-        refresh_token :<br>';
-            /*for ($i=1;strlen($tmptoken)>0;$i++) {
-                $t['t' . $i] = substr($tmptoken,0,128);
-                $str .= '
-            t' . $i . ':<textarea readonly style="width: 95%">' . $t['t' . $i] . '</textarea><br><br>';
-                $tmptoken=substr($tmptoken,128);
-            }
-            $str .= '
-        Add t1-t'.--$i.' to environments.*/
-            $str .= '
-        <textarea readonly style="width: 95%">' . $tmptoken . '</textarea><br><br>
-        '.getconstStr('SavingToken').'
-        <script>
-            var texta=document.getElementsByTagName(\'textarea\');
-            for(i=0;i<texta.length;i++) {
-                texta[i].style.height = texta[i].scrollHeight + \'px\';
-            }
-            document.cookie=\'language=; path=/\';
-            document.cookie=\'disktag=; path=/\';
-        </script>';
-            setConfig([ 'refresh_token' => $tmptoken, 'token_expires' => time()+30*24*60*60 ], $_COOKIE['disktag']);
-            savecache('access_token', $ret['access_token'], $ret['expires_in'] - 60);
-            $str .= '
-            <meta http-equiv="refresh" content="2;URL=' . $url . '">';
-            return message($str, getconstStr('WaitJumpIndex'));
-        }
-        return message('<pre>' . $tmp['body'] . '</pre>', $tmp['stat']);
-        //return message('<pre>' . json_encode($ret, JSON_PRETTY_PRINT) . '</pre>', 500);
-    }
-    if ($_GET['install1']) {
-        $_SERVER['disk_oprating'] = $_COOKIE['disktag'];
-        $_SERVER['disktag'] = $_COOKIE['disktag'];
-        config_oauth();
-        if (getConfig('Onedrive_ver')=='MS' || getConfig('Onedrive_ver')=='CN' || getConfig('Onedrive_ver')=='MSC') {
-            return message('
-    <a href="" id="a1">'.getconstStr('JumptoOffice').'</a>
-    <script>
-        url=location.protocol + "//" + location.host + "'.$url.'";
-        url="'. $_SERVER['oauth_url'] .'authorize?scope='. $_SERVER['scope'] .'&response_type=code&client_id='. $_SERVER['client_id'] .'&redirect_uri='. $_SERVER['redirect_uri'] . '&state=' .'"+encodeURIComponent(url);
-        document.getElementById(\'a1\').href=url;
-        //window.open(url,"_blank");
-        location.href = url;
-    </script>
-    ', getconstStr('Wait').' 1s', 201);
-        }
-    }
-    if ($_GET['install0']) {
-        if ($_POST['disktag_add']!='' && ($_POST['Onedrive_ver']=='MS' || $_POST['Onedrive_ver']=='CN' || $_POST['Onedrive_ver']=='MSC')) {
-            if (in_array($_COOKIE['disktag'], $CommonEnv)) {
-                return message('Do not input ' . $envs . '<br><button onclick="location.href = location.href;">'.getconstStr('Refresh').'</button><script>document.cookie=\'disktag=; path=/\';</script>', 'Error', 201);
-            }
-            $_SERVER['disktag'] = $_COOKIE['disktag'];
-            $tmp['disktag_add'] = $_POST['disktag_add'];
-            $tmp['diskname'] = $_POST['diskname'];
-            $tmp['Onedrive_ver'] = $_POST['Onedrive_ver'];
-            if ($_POST['Onedrive_ver']=='MSC') {
-                $tmp['client_id'] = $_POST['client_id'];
-                $tmp['client_secret'] = $_POST['client_secret'];
-                //$_POST['client_secret'];
-            }
-            $response = json_decode( setConfig($tmp, $_COOKIE['disktag']), true )['Response'];
-            $title = getconstStr('MayinEnv');
-            $html = getconstStr('Wait') . ' 3s<meta http-equiv="refresh" content="3;URL=' . $url . '?AddDisk&install1">';
-            if (isset($response['id'])&&isset($response['message'])) {
-            $html = $response['id'] . '<br>
-' . $response['message'] . '<br><br>
-function_name:' . $_SERVER['function_name'] . '<br>
-<button onclick="location.href = location.href;">'.$constStr['Refresh'][$constStr['language']].'</button>';
-            $title = 'Error';
-            }
-            return message($html, $title, 201);
-        }
-    }
-
-    if ($constStr['language']!='zh-cn') {
-        $linklang='en-us';
-    } else $linklang='zh-cn';
-    $ru = "https://developer.microsoft.com/".$linklang."/graph/quick-start?appID=_appId_&appName=_appName_&redirectUrl=".$_SERVER['redirect_uri']."&platform=option-php";
-    $deepLink = "/quickstart/graphIO?publicClientSupport=false&appName=OneManager&redirectUrl=".$_SERVER['redirect_uri']."&allowImplicitFlow=false&ru=".urlencode($ru);
-    $app_url = "https://apps.dev.microsoft.com/?deepLink=".urlencode($deepLink);
-    $html = '
-    <form action="?AddDisk&install0" method="post" onsubmit="return notnull(this);">
-        '.getconstStr('OnedriveDiskTag').':<input type="text" name="disktag_add" placeholder="' . getconstStr('EnvironmentsDescription')['disktag'] . '" style="width:100%"><br>
-        '.getconstStr('OnedriveDiskName').':<input type="text" name="diskname" placeholder="' . getconstStr('EnvironmentsDescription')['diskname'] . '" style="width:100%"><br>
-        Onedrive_Ver：<br>
-        <label><input type="radio" name="Onedrive_ver" value="MS" checked>MS: '.getconstStr('OndriveVerMS').'</label><br>
-        <label><input type="radio" name="Onedrive_ver" value="CN">CN: '.getconstStr('OndriveVerCN').'</label><br>
-        <label><input type="radio" name="Onedrive_ver" value="MSC" onclick="document.getElementById(\'secret\').style.display=\'\';">MSC: '.getconstStr('OndriveVerMSC').'
-            <div id="secret" style="display:none">
-                <a href="'.$app_url.'" target="_blank">'.getconstStr('GetSecretIDandKEY').'</a><br>
-                client_secret:<input type="text" name="client_secret"><br>
-                client_id:<input type="text" name="client_id" placeholder="12345678-90ab-cdef-ghij-klmnopqrstuv"><br>
-            </div>
-        </label><br>
-        <input type="submit" value="'.getconstStr('Submit').'">
-    </form>
-    <script>
-        function notnull(t)
-        {
-            if (t.disktag_add.value==\'\') {
-                alert(\'Input Disk Tag\');
-                return false;
-            }
-            envs = [' . $envs . '];
-            if (envs.indexOf(t.disktag_add.value)>-1) {
-                alert("Do not input ' . $envs . '");
-                return false;
-            }
-            var reg = /^[a-zA-Z]([-_a-zA-Z0-9]{1,20})$/;
-            if (!reg.test(t.disktag_add.value)) {
-                alert(\''.getconstStr('TagFormatAlert').'\');
-                return false;
-            }
-            document.cookie=\'disktag=\'+t.disktag_add.value+\'; path=/\';
-            return true;
-        }
-    </script>';
-    $title = 'Bind Onedrive';
-    return message($html, $title, 201);
-}
-
 function install()
 {
     global $constStr;
@@ -249,11 +114,8 @@ function install()
                 $tmp['function_name'] = $function_name;
 	        }
             $response = json_decode(setHerokuConfig($tmp, $function_name, $APIKey)['body'], true);
-            if (isset($response['id'])&&isset($response['message'])) {
-                $html = $response['id'] . '<br>
-' . $response['message'] . '<br><br>
-function_name:' . $_SERVER['function_name'] . '<br>
-<button onclick="location.href = location.href;">'.$constStr['Refresh'][$constStr['language']].'</button>';
+            if (api_error($response)) {
+                $html = api_error_msg($response);
                 $title = 'Error';
             } else {
                 return output('Jump<meta http-equiv="refresh" content="3;URL=' . path_format($_SERVER['base_path'] . '/') . '">', 302);
@@ -358,173 +220,25 @@ function updateHerokuapp($function_name, $apikey)
     return HerokuAPI('POST', 'https://api.heroku.com/apps/' . $function_name . '/builds', $data, $apikey);
 }
 
-function EnvOpt($function_name, $needUpdate = 0)
+function api_error($response)
 {
-    global $constStr;
-    global $ShowedCommonEnv;
-    global $ShowedInnerEnv;
-    asort($ShowedCommonEnv);
-    asort($ShowedInnerEnv);
-    $html = '<title>OneManager '.getconstStr('Setup').'</title>';
-    if ($_POST['updateProgram']==getconstStr('updateProgram')) {
-        $response = json_decode(updateHerokuapp(getConfig('function_name'), getConfig('APIKey'))['body'], true);
-        if (isset($response['id'])&&isset($response['message'])) {
-            $html = $response['id'] . '<br>
+    return isset($response['id'])&&isset($response['message']);
+}
+
+function api_error_msg($response)
+{
+    return $response['id'] . '<br>
 ' . $response['message'] . '<br><br>
 function_name:' . $_SERVER['function_name'] . '<br>
-<button onclick="location.href = location.href;">'.$constStr['Refresh'][$constStr['language']].'</button>';
-            $title = 'Error';
-        } else {
-            $html .= getconstStr('UpdateSuccess') . '<br>
 <button onclick="location.href = location.href;">'.getconstStr('Refresh').'</button>';
-            $title = getconstStr('Setup');
-        }
-        return message($html, $title);
-    }
-    if ($_POST['submit1']) {
-        foreach ($_POST as $k => $v) {
-            if (in_array($k, $constEnv)) {
-                if (!(getConfig($k)==''&&$v=='')) $tmp[$k] = $v;
-            }
-        }
-        $_SERVER['disk_oprating'] = '';
-        foreach ($_POST as $k => $v) {
-            if (in_array($k, $ShowedCommonEnv)) {
-                if (!(getConfig($k)==''&&$v=='')) $tmp[$k] = $v;
-            }
-            if (in_array($k, $ShowedInnerEnv)||$k=='disktag_del' || $k=='disktag_add') {
-                $tmp[$k] = $v;
-            }
-            if ($k == 'disk') $_SERVER['disk_oprating'] = $v;
-        }
-        /*if ($tmp['domain_path']!='') {
-            $tmp1 = explode("|",$tmp['domain_path']);
-            $tmparr = [];
-            foreach ($tmp1 as $multidomain_paths){
-                $pos = strpos($multidomain_paths,":");
-                if ($pos>0) $tmparr[substr($multidomain_paths, 0, $pos)] = path_format(substr($multidomain_paths, $pos+1));
-            }
-            $tmp['domain_path'] = $tmparr;
-        }*/
-        $response = json_decode(setConfig($tmp, $_SERVER['disk_oprating'])['body'], true);
-        if (isset($response['id'])&&isset($response['message'])) {
-            $html = $response['id'] . '<br>
-' . $response['message'] . '<br><br>
-function_name:' . $_SERVER['function_name'] . '<br>
-<button onclick="location.href = location.href;">'.$constStr['Refresh'][$constStr['language']].'</button>';
-            $title = 'Error';
-        } else {
-            $html .= '<script>location.href=location.href</script>';
-        }
-    }
-    if ($_GET['preview']) {
-        $preurl = $_SERVER['PHP_SELF'] . '?preview';
-    } else {
-        $preurl = path_format($_SERVER['PHP_SELF'] . '/');
-    }
-    $html .= '
-<a href="'.$preurl.'">'.getconstStr('Back').'</a>&nbsp;&nbsp;&nbsp;<a href="'.$_SERVER['base_path'].'">'.getconstStr('Back').getconstStr('Home').'</a><br>
-<a href="https://github.com/qkqpttgf/OneManager-php">Github</a><br>
-<form action="" method="post">
-';
-    if ($needUpdate) {
-        $html .= '<pre>' . $_SERVER['github_version'] . '</pre>';
-    } else {
-        $html .= getconstStr('NotNeedUpdate');
-    }
-    $html .= '
-    <input type="submit" name="updateProgram" value="'.getconstStr('updateProgram').'">
-</form>
-<table border=1 width=100%>
-    <form name="common" action="" method="post">
-        <tr>
-            <td colspan="2">'.getconstStr('PlatformConfig').'</td>
-        </tr>';
-    foreach ($ShowedCommonEnv as $key) {
-        if ($key=='language') {
-            $html .= '
-        <tr>
-            <td><label>' . $key . '</label></td>
-            <td width=100%>
-                <select name="' . $key .'">';
-            foreach ($constStr['languages'] as $key1 => $value1) {
-                $html .= '
-                    <option value="'.$key1.'" '.($key1==getConfig($key)?'selected="selected"':'').'>'.$value1.'</option>';
-            }
-            $html .= '
-                </select>
-            </td>
-        </tr>';
-        } elseif ($key=='theme') {
-            $theme_arr = scandir('theme');
-            $html .= '
-        <tr>
-            <td><label>' . $key . '</label></td>
-            <td width=100%>
-                <select name="' . $key .'">
-                <option value=""></option>';
-            foreach ($theme_arr as $v1) {
-                if ($v1!='.' && $v1!='..') $html .= '
-                    <option value="'.$v1.'" '.($v1==getConfig($key)?'selected="selected"':'').'>'.$v1.'</option>';
-            }
-            $html .= '
-                </select>
-            </td>
-        </tr>';
-        } /*elseif ($key=='domain_path') {
-            $tmp = getConfig($key);
-            $domain_path = '';
-            foreach ($tmp as $k1 => $v1) {
-                $domain_path .= $k1 . ':' . $v1 . '|';
-            }
-            $domain_path = substr($domain_path, 0, -1);
-            $html .= '
-        <tr>
-            <td><label>' . $key . '</label></td>
-            <td width=100%><input type="text" name="' . $key .'" value="' . $domain_path . '" placeholder="' . getconstStr('EnvironmentsDescription')[$key] . '" style="width:100%"></td>
-        </tr>';
-        }*/ else $html .= '
-        <tr>
-            <td><label>' . $key . '</label></td>
-            <td width=100%><input type="text" name="' . $key .'" value="' . getConfig($key) . '" placeholder="' . getconstStr('EnvironmentsDescription')[$key] . '" style="width:100%"></td>
-        </tr>';
-    }
-    $html .= '
-        <tr><td><input type="submit" name="submit1" value="'.getconstStr('Setup').'"></td></tr>
-    </form>
-</table><br>';
-    foreach (explode("|",getConfig('disktag')) as $disktag) {
-        if ($disktag!='') {
-            $html .= '
-<table border=1 width=100%>
-    <form action="" method="post">
-        <tr>
-            <td colspan="2">'.$disktag.'：
-            <input type="hidden" name="disktag_del" value="'.$disktag.'">
-            <input type="submit" name="submit1" value="'.getconstStr('DelDisk').'">
-            </td>
-        </tr>
-    </form>';
-            if (getConfig('refresh_token', $disktag)!='') {
-                $html .= '
-    <form name="'.$disktag.'" action="" method="post">
-        <input type="hidden" name="disk" value="'.$disktag.'">';
-                foreach ($ShowedInnerEnv as $key) {
-                    $html .= '
-        <tr>
-            <td><label>' . $key . '</label></td>
-            <td width=100%><input type="text" name="' . $key .'" value="' . getConfig($key, $disktag) . '" placeholder="' . getconstStr('EnvironmentsDescription')[$key] . '" style="width:100%"></td>
-        </tr>';
-                }
-                $html .= '
-        <tr><td><input type="submit" name="submit1" value="'.getconstStr('Setup').'"></td></tr>
-    </form>';
-            }
-            $html .= '
-</table><br>';
-        }
-    }
-    $html .= '
-<a href="?AddDisk">'.getconstStr('AddDisk').'</a>';
-    return message($html, getconstStr('Setup'));
+}
+
+function OnekeyUpate()
+{
+    return json_decode(updateHerokuapp(getConfig('function_name'), getConfig('APIKey'))['body'], true);
+}
+
+function setConfigResponse($response)
+{
+    return json_decode( $response['body'], true );
 }
