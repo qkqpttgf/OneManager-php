4f526c589cbd152691d9212b3b34072c3d2fd115
BingoKingo <h6h4m123@yeah.net>
2020-04-02 22:41:41 +0800

    CSS

--- theme/classic.php
+++ theme/classic.php
@@ -16,7 +16,7 @@
         .changelanguage{position:absolute;right:5px;}
         .title{text-align:center;margin-top:1rem;letter-spacing:2px;margin-bottom:2rem}
         .title a{color:#333;text-decoration:none}
-        .list-wrapper{width:96%;margin:0 auto 30px;position:relative;box-shadow:0 0 6px 0 rgb(128,128,128);border-radius:6px;}
+        .list-wrapper{width:96%;margin:0 auto 10px;position:relative;box-shadow:0 0 6px 0 rgb(128,128,128);border-radius:6px;}
         .list-container{position:relative;overflow:hidden;border-radius:6px;}
         .list-header-container{position:relative}
         .list-header-container a.back-link{color:#000;display:inline-block;position:absolute;font-size:16px;margin:20px 10px;padding:10px 10px;vertical-align:middle;text-decoration:none}
@@ -25,14 +25,14 @@
         .list-body-container{position:relative;left:0;overflow-x:hidden;overflow-y:auto;box-sizing:border-box;background:rgba(245,245,245,0.5)}
         .more-disk{margin:0;border:0 none;padding:30px 30px;text-align:left;font-weight:400;color:#000;background-color:rgba(245,245,245,0.5);white-space:nowrap;overflow:auto;}
         .more-disk a{margin:0 10px;padding:5px;transition-duration: 0.4s;border-radius: 6px; background-color: white; color: black; border: 2px solid rgba(85,85,85,0.7); }
-        .more-disk a:hover, .more-disk a[now]{ background-color: rgba(85,85,85,0.2); color: white; }
+        .more-disk a:hover, .more-disk a[now]{ background-color: rgba(85,85,85,0.4); color: white; }
         .list-table{width:100%;padding:0 20px 20px 20px;border-spacing:0}
         .list-table tr{height:32px}
-        .list-table tr[data-to]:hover{background:rgba(85,85,85,0.2);color:white;}
+        .list-table tr[data-to]:hover{background:rgba(85,85,85,0.4);color:white;}
         .list-table tr[data-to]:hover a{color:white}
         .list-table tr:first-child{background:rgba(245,245,245,0)}
-        .list-table td,.list-table th{padding:0 10px;text-align:left}
-        .list-table .size,.list-table .updated_at{text-align:right}
+        .list-table td,.list-table th{padding:0 8px;text-align:left}
+        .list-table .size,.list-table .updated_at{text-align:right;width:15% !important}
         .mask{position:absolute;left:0px;top:0px;width:100%;background-color:#000;filter:alpha(opacity=20);opacity:0.2;z-index:2;}
 <?php if ($_SERVER['admin']) { ?>
         .operate{display:inline-table;margin:0;margin-right:5px;list-style:none}
