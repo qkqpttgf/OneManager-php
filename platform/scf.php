00023986b3386bf14c5a3946e00eb1dbd7b9ac2f
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-04-02 18:21:00 +0800

    use accept_language

--- platform/scf.php
+++ platform/scf.php
@@ -28,6 +28,7 @@ function GetGlobalVariable($event)
 
 function GetPathSetting($event, $context)
 {
+    $_SERVER['firstacceptlanguage'] = strtolower(splitfirst(splitfirst($event['headers']['accept-language'],';')[0],',')[0]);
     $_SERVER['function_name'] = $context['function_name'];
     $_SERVER['namespace'] = $context['namespace'];
     $host_name = $event['headers']['host'];
@@ -132,7 +133,6 @@ function install()
         setConfig($tmp);
         if (needUpdate()) {
             OnekeyUpate();
-            //updateProgram($_SERVER['function_name'], $_SERVER['Region'], $_SERVER['namespace'], $SecretId, $SecretKey);
             return message('update to github version, reinstall.<script>document.cookie=\'language=; path=/\';</script><meta http-equiv="refresh" content="3;URL=' . $url . '">', 'Program updating', 201);
         }
         return output('Jump<script>document.cookie=\'language=; path=/\';</script><meta http-equiv="refresh" content="3;URL=' . path_format($_SERVER['base_path'] . '/') . '">', 302);
