b70cd48585da60558659999451dbc577338b5a9b
BingoKingo <h6h4m123@yeah.net>
2020-04-03 20:31:45 +0800

    CSS

--- theme/classic.php
+++ theme/classic.php
@@ -16,7 +16,7 @@
 	::-webkit-scrollbar{display:none}
         .changelanguage{position:absolute;right:5px;}
         .title{text-align:center;margin-top:1rem;letter-spacing:2px;margin-bottom:2rem}
-        .title a{color:#333;text-decoration:none}
+        .title a{color:#24292e;text-decoration:none}
         .list-wrapper{width:88%;margin:0 auto 10px;position:relative;box-shadow:0 0 6px 0 rgb(128,128,128);border-radius:6px;}
         .list-container{position:relative;overflow:hidden;border-radius:6px;}
         .list-header-container{position:relative}
@@ -25,8 +25,8 @@
         .table-header{margin:0;border:0 none;padding:30px 60px;text-align:left;font-weight:400;color:#000;background-color:rgba(245,245,245,0.3);word-break: break-all;word-wrap: break-word;}
         .list-body-container{position:relative;left:0;overflow-x:hidden;overflow-y:auto;box-sizing:border-box;background:rgba(245,245,245,0.3)}
         .more-disk{margin:0;border:0 none;padding:18px 18px;text-align:center;font-weight:400;color:#000;background-color:rgba(245,245,245,0.3);white-space:nowrap;overflow:auto;}
-        .more-disk a{margin:0 2px;padding:5px;transition-duration: 0.3s;border-radius: 12px; background-color: white; color: #24292e}
-        .more-disk a:hover, .more-disk a[now]{ background-color: rgba(85,85,85,0.4); color: white; }
+        .more-disk a{margin:0 2px;padding:5px;transition-duration: 0.3s;border-radius: 18px; background-color:transparent; color: #24292e}
+        .more-disk a:hover, .more-disk a[now]{ background-color:rgba(85,85,85,0.4); color: white; }
         .list-table{width:100%;padding:0 20px 20px 20px;border-spacing:0}
         .list-table tr{height:32px;transition-duration: 0.3s}
         .list-table tr[data-to]:hover{background:rgba(85,85,85,0.4);color:white}
@@ -37,7 +37,7 @@
         .mask{position:absolute;left:0px;top:0px;width:100%;background-color:#000;filter:alpha(opacity=20);opacity:0.2;z-index:2;}
 <?php if ($_SERVER['admin']) { ?>
         .operate{display:inline-table;margin:0;margin-right:5px;list-style:none}
-        .operate ul{position:absolute;display:none;background:rgba(85,85,85,0.4);border-radius:6px;margin:-7px 0 0 0;padding:0 3px;color:#205D67;z-index:3;}
+        .operate ul{position:absolute;display:none;background:rgba(85,85,85,0.8);transition-duration: 0.3s;border-radius:6px;margin:-7px 0 0 0;padding:0 3px;color:#205D67;z-index:3;}
         .operate:hover ul{position:absolute;display:inline-table;}
         .operate ul li{padding:7px;list-style:none;display:block;}
         .list-table tr[data-to]:hover .operate ul li a{color:black}
