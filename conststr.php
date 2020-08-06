0dac33703707014cee65d208f76cc9206e2f6ec9
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-08-06 10:47:19 +0800

    SCF:select save in env or file

--- conststr.php
+++ conststr.php
@@ -792,4 +792,12 @@ $constStr = [
         'en-us' => 'Query Branchs',
         'zh-cn' => '查询分支',
     ],
+    'ONEMANAGER_CONFIG_SAVE_ENV' => [
+        'en-us' => 'Config save in Environments, max add 4 disks',
+        'zh-cn' => '配置保存在环境变量，最多添加4个盘',
+    ],
+    'ONEMANAGER_CONFIG_SAVE_FILE' => [
+        'en-us' => 'Config save in code file, may cause fee',
+        'zh-cn' => '配置保存在代码文件中，可能产生费用',
+    ],
 ];
