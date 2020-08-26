1b04aafd95501ec611294a09322ed56040c5f668
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-08-26 17:58:45 +0800

    add new 21Vianet id&secret

--- common.php
+++ common.php
@@ -611,10 +611,12 @@ function config_oauth()
         if (getConfig('usesharepoint')=='on') $_SERVER['api_url'] = 'https://graph.microsoft.com/v1.0/sites/' . getConfig('siteid') . '/drive/root';
     }
     if (getConfig('Drive_ver')=='CN') {
-        // CN
+        // CN 21Vianet
         // https://portal.azure.cn
         $_SERVER['client_id'] = '04c3ca0b-8d07-4773-85ad-98b037d25631';
         $_SERVER['client_secret'] = 'h8@B7kFVOmj0+8HKBWeNTgl@pU/z4yLB';
+        //$_SERVER['client_id'] = 'b15f63f5-8b72-48b5-af69-8cab7579bff7';
+        //$_SERVER['client_secret'] = '0IIuZ1Kcq_YI3NrkZFwsniEo~BoP~8_M22';
         $_SERVER['oauth_url'] = 'https://login.partner.microsoftonline.cn/common/oauth2/v2.0/';
         $_SERVER['api_url'] = 'https://microsoftgraph.chinacloudapi.cn/v1.0/me/drive/root';
         $_SERVER['scope'] = 'https://microsoftgraph.chinacloudapi.cn/Files.ReadWrite.All offline_access';
