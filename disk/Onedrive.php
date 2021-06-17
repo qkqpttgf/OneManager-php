86ba05b5be361602d842c9e5143f9da9bbe20b91
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2021-06-17 17:18:44 +0800

    MSAPI, GET

--- disk/Onedrive.php
+++ disk/Onedrive.php
@@ -950,7 +950,9 @@ class Onedrive {
                 $url .= ':' . $path;
                 if (substr($url,-1)=='/') $url=substr($url,0,-1);
             }
-            if ($method=='PUT') {
+            if ($method=='GET') {
+                $method = 'GET'; // do nothing
+            } elseif ($method=='PUT') {
                 if ($path=='' or $path=='/') {
                     $url .= 'content';
                 } else {
